b3dad727244a1efc6281efb9baa6aee138f2eec6
Jamie Paisley <jamiepaisley@Jamies-MacBook-Pro.local>
2017-10-10 13:08:34 -0400

    OutputHandler's CREATETITLE,isNumber and isInteger function

---
+++ src/server/logic/handler/OutputHandler.java
@@ -0,0 +1,83 @@
+package server.logic.handler;
+
+
+import server.logic.handler.model.Output;
+import server.logic.tables.TitleTable;
+import server.logic.tables.UserTable;
+
+public class OutputHandler {
+	public static final int WAITING = 0;
+	public static final int FINISHWAITING=1;
+    public static final int CLERK = 2;
+    public static final int USER = 3;
+    public static final int CREATEUSER=4;
+    public static final int CREATETITLE=5;
+    
+    public Output createUser(String input) {
+		Output output=new Output("",0);
+		String[] strArray = null;   
+        strArray = input.split(",");
+        boolean email=strArray[0].contains("@");
+        Object result="";
+        if(strArray.length!=2 || email!=true){
+        	output.setOutput("Your input should in this format:'username,password'");
+        	output.setState(CREATEUSER);
+        }else{
+        	result=UserTable.getInstance().createuser(strArray[0], strArray[1]);
+        	if(result.equals(true)){
+        		output.setOutput("Success!");
+        	}else{
+        		output.setOutput("The User Already Exists!");
+        	}
+        	output.setState(CLERK);
+        }
+		return output;
+	}
+    
+    public Output createTitle(String input) {
+		Output output=new Output("",0);
+		String[] strArray = null;   
+        strArray = input.split(",");
+        boolean number=isInteger(strArray[0]);
+        Object result="";
+        if(strArray.length!=2 || number!=true){
+        	output.setOutput("Your input should in this format:'ISBN,title',ISBN should be a 13-digit number");
+        	output.setState(CREATETITLE);
+        }else{
+        	result=TitleTable.getInstance().createtitle(strArray[0], strArray[1]);
+        	if(result.equals(true)){
+        		output.setOutput("Success!");
+        	}else{
+        		output.setOutput("The Title Already Exists!");
+        	}
+        	output.setState(CLERK);
+        }
+		return output;
+	}
+    
+    
+    
+    public static boolean isInteger(String value) {
+		char[] ch = value.toCharArray();
+		boolean isNumber=true;
+		if(value.length()==13){
+			for (int i = 0; i < ch.length; i++) {
+				isNumber = Character.isDigit(ch[i]);
+			}
+		}else{
+			isNumber=false;
+		}
+		return isNumber;
+		 }
+	
+	public boolean isNumber(String value) {
+		char[] ch = value.toCharArray();
+		boolean isNumber=true;
+			for (int i = 0; i < ch.length; i++) {
+				isNumber = Character.isDigit(ch[i]);
+			}
+		return isNumber;
+	}
+	
+	
+}
--- src/testing/OutputHandlerTest.java
+++ src/testing/OutputHandlerTest.java
@@ -8,18 +8,23 @@ import org.junit.Before;
 import org.junit.Test;
 
 import server.logic.handler.model.Output;
-import server.logic.handlers.OutputHandler;
+import server.logic.handler.OutputHandler;
+import server.logic.model.Title;
 import server.logic.model.User;
+import server.logic.tables.TitleTable;
 import server.logic.tables.UserTable;
 
 public class OutputHandlerTest {
 	UserTable userTable;
 	OutputHandler outputhandler;
+	TitleTable titleTable;
+	
 	
 	@Before 
 	public void setup() throws Exception {
 		userTable = UserTable.getInstance();
 		outputhandler = new OutputHandler();
+		titleTable = TitleTable.getInstance();
 	}
 	
 	@Test
@@ -34,5 +39,37 @@ public class OutputHandlerTest {
 		assertEquals("jamie", users.get(users.size()-1).getPassword());
 	}
 	
+	@Test 
+	public void isNumberTest() {
+		//pass
+		assertEquals(true, outputhandler.isNumber("123456789"));
+ 		//fail
+		assertEquals(false, outputhandler.isNumber("x"));
+		assertEquals(false, outputhandler.isNumber("@"));
+		
+	}
+	
+	@Test 
+	public void isIntegerTest() {
+		//pass
+		assertEquals(true, outputhandler.isInteger("1234567890123"));
+ 		//fail
+		assertEquals(false, outputhandler.isInteger("123456789012"));
+		assertEquals(false, outputhandler.isInteger("x"));
+		assertEquals(false, outputhandler.isInteger("@"));
+		
+	}
+	
+	@Test
+	public void createTitleTest() {
+		Output jamie = outputhandler.createTitle("1234567890123,The old man and the Sea");
+		
+		assertEquals(2, jamie.getState());
+		assertEquals("Success!", jamie.getOutput());
+		
+		List<Title> titles = titleTable.getTitleTable();
+		assertEquals("1234567890123", titles.get(titles.size()-1).getISBN());
+		assertEquals("The old man and the Sea", titles.get(titles.size()-1).getBooktitle());
+	}
 	
 }
